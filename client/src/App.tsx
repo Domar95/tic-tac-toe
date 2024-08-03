11e915e2b20466cee4966059ecb86de102636880
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-03 20:45:59 +0200

    feat: add tic tac toe game
    based on react tutorial: https://react.dev/learn/tutorial-tic-tac-toe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "./components/common";
+import Game from "./components/tic-tac-toe-game/Game";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Game />
     </div>
   );
 }
