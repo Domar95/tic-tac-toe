b655039ab2aa2e72e14f9f356098d04f0fdc32bb
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-11 04:09:05 +0200

    refactor: move content into main content component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,13 @@
-import { Box, Container } from "@mui/material";
 import { Navbar } from "./components/common";
 import { ConnectionProvider } from "hooks/ConnectionContext";
 import MainContent from "components/main-content/MainContent";
-import checker from "assets/checker.png";
-import HeroExtension from "components/main-content/hero/HeroExtension";
 
 function App() {
   return (
     <div>
       <ConnectionProvider>
         <Navbar />
-        <Box
-          sx={{
-            backgroundImage: `url(${checker})`,
-            backgroundSize: "auto",
-            backgroundPosition: "center",
-          }}
-        >
-          <Container maxWidth="sm">
-            <MainContent />
-          </Container>
-        </Box>
-        <Container maxWidth="lg">
-          <HeroExtension />
-        </Container>
+        <MainContent />
       </ConnectionProvider>
     </div>
   );
