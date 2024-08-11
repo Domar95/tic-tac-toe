342a4eab906cd7b4c3cda55ff1859ed990b8a1c3
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-11 03:57:54 +0200

    feat: add background image

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,25 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { Navbar } from "./components/common";
 import { ConnectionProvider } from "hooks/ConnectionContext";
 import MainContent from "components/main-content/MainContent";
+import checker from "assets/checker.png";
 
 function App() {
   return (
     <div>
       <ConnectionProvider>
         <Navbar />
-        <Container maxWidth="sm">
-          <MainContent />
-        </Container>
+        <Box
+          sx={{
+            backgroundImage: `url(${checker})`,
+            backgroundSize: "auto",
+            backgroundPosition: "center",
+          }}
+        >
+          <Container maxWidth="sm">
+            <MainContent />
+          </Container>
+        </Box>
       </ConnectionProvider>
     </div>
   );
