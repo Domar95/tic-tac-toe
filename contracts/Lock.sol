5816843dcb1a7dd6a8ea63fc8a5f6381c4804b83
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-11 15:17:19 +0200

    build(hardhat): update config
    add support for Sepolia testnet

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
+// Author: @domar95
 contract Lock {
     uint public unlockTime;
     address payable public owner;
