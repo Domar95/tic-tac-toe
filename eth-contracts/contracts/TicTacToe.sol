c8409e0bc871428dab7b0a930bbe880cc20d8014
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-17 22:08:32 +0200

    feat(tic-tac-toe): add waitingForPlayers state and restrict moves to expected player

--- eth-contracts/contracts/TicTacToe.sol
+++ eth-contracts/contracts/TicTacToe.sol
@@ -4,24 +4,45 @@ pragma solidity ^0.8.0;
 // Author: @domar95
 contract TicTacToe {
     enum GameState {
+        WaitingForPlayers,
         NextX,
         NextO,
         WinnerX,
         WinnerO,
         Draw
     }
-    GameState private gameState = GameState.NextX;
+    GameState private gameState = GameState.WaitingForPlayers;
     uint8[9] private currentBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    address private playerX;
+    address private playerO;
 
     event GameStateUpdated(GameState, uint8[9] currentBoard);
 
+    function joinGame() public {
+        require(
+            gameState == GameState.WaitingForPlayers,
+            "Game already started"
+        );
+        require(playerX != msg.sender, "You have already joined");
+
+        if (playerX == address(0)) {
+            playerX = msg.sender;
+        } else {
+            playerO = msg.sender;
+            gameState = GameState.NextX;
+        }
+
+        emit GameStateUpdated(gameState, currentBoard);
+    }
+
     function handlePlay(uint8 index) public {
+        require(
+            (gameState == GameState.NextX && msg.sender == playerX) ||
+                (gameState == GameState.NextO && msg.sender == playerO),
+            "Invalid player or game state"
+        );
         require(index < 9, "Index out of bounds");
         require(currentBoard[index] == 0, "Square already filled");
-        require(
-            gameState == GameState.NextX || gameState == GameState.NextO,
-            "Game already ended"
-        );
 
         currentBoard[index] = gameState == GameState.NextX ? 1 : 2;
         updateGameState();
@@ -29,7 +50,9 @@ contract TicTacToe {
 
     function resetBoard() public {
         currentBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-        gameState = GameState.NextX;
+        gameState = GameState.WaitingForPlayers;
+        playerX = address(0);
+        playerO = address(0);
 
         emit GameStateUpdated(gameState, currentBoard);
     }
@@ -92,5 +115,13 @@ contract TicTacToe {
 
     function getCurrentBoard() public view returns (uint8[9] memory) {
         return currentBoard;
+    }
+
+    function getPlayerX() public view returns (address) {
+        return playerX;
+    }
+
+    function getPlayerO() public view returns (address) {
+        return playerO;
     }
 }
