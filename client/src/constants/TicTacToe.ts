be9f4f13f57c1d49273a50391bd28ceaee8ce459
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-17 19:48:37 +0200

    feat: add constants for contract addresses and abi

---
+++ client/src/constants/TicTacToe.ts
@@ -0,0 +1,4 @@
+import TicTacToe from "contracts/TicTacToe.sol/TicTacToe.json";
+
+export const TIC_TAC_TOE_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const TIC_TAC_TOE_ABI = TicTacToe.abi;
