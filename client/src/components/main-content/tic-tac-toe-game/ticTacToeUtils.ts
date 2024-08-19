df53648224f8c822132f14a9f94a73e84eb90c8d
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-19 03:16:18 +0200

    feat(tic-tac-toe): integrate waiting for players state

--- client/src/components/main-content/tic-tac-toe-game/ticTacToeUtils.ts
+++ client/src/components/main-content/tic-tac-toe-game/ticTacToeUtils.ts
@@ -1,14 +1,16 @@
 export const renderGameState = (gameState: BigInt): string => {
   switch (gameState) {
     case BigInt(0):
-      return "Next player: X";
+      return "Waiting for players to join...";
     case BigInt(1):
-      return "Next player: O";
+      return "Next player: X";
     case BigInt(2):
-      return "Player X wins!";
+      return "Next player: O";
     case BigInt(3):
-      return "Player O wins!";
+      return "Player X wins!";
     case BigInt(4):
+      return "Player O wins!";
+    case BigInt(5):
       return "It's a draw!";
     default:
       return "Unknown game state";
