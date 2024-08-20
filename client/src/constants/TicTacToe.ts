3e9632bd2871322af122b8f81952ddb6bb1ade4a
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-20 02:12:01 +0200

    chore: format file

--- client/src/constants/TicTacToe.ts
+++ client/src/constants/TicTacToe.ts
@@ -1,5 +1,5 @@
 import TicTacToeArtifact from "contracts/TicTacToe.json";
-import contractAddress from "contracts/contract-address.json"
+import contractAddress from "contracts/contract-address.json";
 
 export const TIC_TAC_TOE_ADDRESS = contractAddress.ticTacToe;
 export const TIC_TAC_TOE_ABI = TicTacToeArtifact.abi;
