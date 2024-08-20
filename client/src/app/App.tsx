6f4ad58a3feb3a67fdb57210a4ad3c93ec550b17
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-20 13:39:40 +0200

    refactor: move all routes into separate file and use in app

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,20 +1,10 @@
-import LandingRoute from "app/routes/Landing";
-import { Route, Routes } from "react-router-dom";
-import { Paths } from "constants/paths";
-import LeaderboardRoute from "app/routes/Leaderboard";
-import ContractRoute from "app/routes/Contract";
-import AboutRoute from "app/routes/About";
 import { AppProvider } from "./Provider";
+import { AppRouter } from "./Router";
 
 function App() {
   return (
     <AppProvider>
-      <Routes>
-        <Route path={Paths.HOME} element={<LandingRoute />} />
-        <Route path={Paths.LEADERBOARD} element={<LeaderboardRoute />} />
-        <Route path={Paths.CONTRACT} element={<ContractRoute />} />
-        <Route path={Paths.ABOUT} element={<AboutRoute />} />
-      </Routes>
+      <AppRouter></AppRouter>
     </AppProvider>
   );
 }
