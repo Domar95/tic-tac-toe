888d237bb556540474111f6b3d35137fb395ee60
Wiktor Domaradzki <87876261+Domar95@users.noreply.github.com>
2024-08-27 00:41:08 +0200

    feat: add polygon testnet to hardhat config
    polygon zkevm cardona testnet: https://cardona-zkevm.polygonscan.com/

--- eth-contracts/hardhat.config.ts
+++ eth-contracts/hardhat.config.ts
@@ -5,6 +5,7 @@ import { vars } from "hardhat/config";
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+const CARDONA_PRIVATE_KEY = vars.get("CARDONA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -12,6 +13,10 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+    cardona: {
+      url: `https://rpc.cardona.zkevm-rpc.com`,
+      accounts: [CARDONA_PRIVATE_KEY],
     },
   },
   etherscan: {
